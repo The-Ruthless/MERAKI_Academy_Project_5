58557c13050927b6d98e6ba67aacad19c6749716
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-06-30 00:07:12 +0300

    finished controller

--- backend/routers/controllers/search.js
+++ backend/routers/controllers/search.js
@@ -53,7 +53,35 @@ const searchByCategory = (req, res) => {
   });
 };
 
+const searchBySubCategory = (req, res) => {
+  const query = `SELECT 
+        advertisements.id,
+        advertisements.title,
+        advertisements.description,
+        advertisements.price,
+        advertisements.image,
+        advertisements.published_at,
+        sub_categories.sub_category,
+        main_categories.main_category,
+        users.id,
+        users.full_name
+        FROM trading_website.advertisements 
+        INNER JOIN sub_categories ON advertisements.sub_category_id= sub_categories.id
+        INNER JOIN main_categories ON sub_categories.main_category_id= main_categories.id
+        INNER JOIN users ON advertisements.user_id= users.id
+        WHERE sub_categories.sub_category='${req.query.subcat}'
+        AND advertisements.title LIKE '%${req.query.keyword}%'
+        ORDER BY published_at DESC`;
+  //http://localhost:5000/search/subcat?subcat=birds&keyword=coc
+
+  db.query(query, (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+};
+
 module.exports = {
   searchAll,
   searchByCategory,
+  searchBySubCategory,
 };
