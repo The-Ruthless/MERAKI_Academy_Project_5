ad1aefabda143256452404662c997caca7cf59f8
Hasan Alsharif <hasanxalsharif@gmail.com>
2021-06-29 12:10:22 +0200

    login modfinal

--- backend/routers/routes/auth/login.js
+++ backend/routers/routes/auth/login.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const loginRouter = express.Router();
-
+// require the controller
 const {login} = require("../../controllers/auth/login");
 
 // http://localhost:5000/login
