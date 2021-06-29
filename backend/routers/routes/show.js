0bed63d19d452b6cca0a554e5eb37ca9dba381ab
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-06-29 21:21:08 +0300

    finished route

--- backend/routers/routes/show.js
+++ backend/routers/routes/show.js
@@ -3,8 +3,10 @@ const showRouter = express.Router();
 
 const {
   showAllCategories,
+  showByCategory,
 } = require("../controllers/show");
 
 showRouter.get("/all", showAllCategories);
+showRouter.get("/mainCat/:category", showByCategory);
 
 module.exports = showRouter;
