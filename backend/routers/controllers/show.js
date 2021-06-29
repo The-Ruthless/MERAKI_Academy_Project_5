2d80428352aeff9f61e69e0f73ff93572035ed50
Obada Alkhalaileh <alkhalaileh.obada@gmail.com>
2021-06-29 21:19:52 +0300

    finished controller function

--- backend/routers/controllers/show.js
+++ backend/routers/controllers/show.js
@@ -24,6 +24,32 @@ const showAllCategories = (req, res) => {
   });
 };
 
+const showByCategory = (req, res) => {
+  const query = `SELECT 
+  advertisements.id,
+  advertisements.title,
+  advertisements.description,
+  advertisements.price,
+  advertisements.image,
+  advertisements.published_at,
+  sub_categories.sub_category,
+  main_categories.main_category,
+  users.id,
+  users.full_name
+  FROM trading_website.advertisements 
+  INNER JOIN sub_categories ON advertisements.sub_category_id= sub_categories.id
+  INNER JOIN main_categories ON sub_categories.main_category_id= main_categories.id
+  INNER JOIN users ON advertisements.user_id= users.id
+  WHERE main_categories.main_category='${req.params.category}'
+  ORDER BY published_at DESC`;
+
+  db.query(query, (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+};
+
 module.exports = {
   showAllCategories,
+  showByCategory,
 };
