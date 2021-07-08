6513823ee6086a4e7b640a5ac2345953a675a84c
Hasan Alsharif <hasanxalsharif@gmail.com>
2021-07-08 16:23:11 +0200

    edit data base and login token - user id

--- backend/routers/controllers/auth/login.js
+++ backend/routers/controllers/auth/login.js
@@ -5,11 +5,14 @@ const bcrypt = require("bcrypt");
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const query = `SELECT * FROM users
+    const query = `SELECT users.id , users.is_deleted, users.email , users.password, users.full_name, users.age, users.gender, 
+    users.nationality , users.phone_number , users.current_location , users.role_id , roles.role
+    FROM users
     INNER JOIN roles ON users.role_id = roles.id
     WHERE email = "${email}";`;
 
     db.query(query, async (err, result) => {
+      console.log(`result`, result[0])
       if (err) throw err;
       //the result contains all data of the user of that specific email : name , mail , pass ...etc
       if (!result[0]) return res.status(400).json("The email doesn't exist");
@@ -17,9 +20,16 @@ const login = async (req, res) => {
       if (valid) {
         const payload = {
           userId: result[0].id,
+          email : result[0].email,
           full_name: result[0].full_name,
-          current_location: result[0].current_location,
+          role_id: result[0].role_id,
           role: result[0].role,
+
+          age : result[0].age,
+          gender : result[0].gender,
+          nationality : result[0].nationality,
+          phone_number : result[0].phone_number,
+          current_location: result[0].current_location,
         };
         const options = {
           expiresIn: "60m",
